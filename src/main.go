afa722e787fb23683047b4250d4ed88dd2b99bd0
Richard Baah <richardbaah@MacBook-Air-de-Richard.local>
2025-08-29 14:53:07 -0400

    set up redis connection as well as some utility functions for making logging server info easier. Exposed API endpoint for server stats. Need to refactor code outside of main functions for sake of readability

--- src/main.go
+++ src/main.go
@@ -2,20 +2,31 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
 	"hash/maphash"
 	"io"
 	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
+	"sync"
 	"time"
 
+	"github.com/joho/godotenv"
+	"github.com/redis/go-redis/v9"
 	"gopkg.in/yaml.v3"
 )
 
 const (
 	communicationPort = "8989"
+)
+
+var (
+	cache *redis.Client
+	once  sync.Once
 )
 
 type ApplicationsServers struct {
@@ -247,21 +258,158 @@ func (rp *responseTime) balence(request chan rawRequest, servers *serverConfig) 
 func (rp *responseTime) name() string {
 	return "response time"
 }
+
 func handleStream(lb loadbalencer, requestStream chan rawRequest, servers *serverConfig) {
 	lb.balence(requestStream, servers)
 }
+func stats(w http.ResponseWriter, r *http.Request) {
+	// tbd implement stats endpoint
+	metrics := newMetricsLogger()
+	for {
+		// just read from redis instance and transform to json and transmit it
+		w.Write([]byte("stats endpoint is under construction"))
+		v := metrics.stats()
+		err := json.NewEncoder(w).Encode(v)
+		if err != nil {
+			fmt.Println(err)
+		}
+		time.Sleep(5 * time.Second) // Reduce this very heavily
+
+	}
+}
+
+// need to add wrapper methods on this for easier logging
+func cacheClient() *redis.Client {
+
+	once.Do(func() {
+		ctx := context.Background()
+		rdb := redis.NewClient(&redis.Options{
+			Addr:     os.Getenv("REDIS_ADDR"),
+			Username: os.Getenv("REDIS_USER"),
+			Password: os.Getenv("REDIS_PASSWORD"),
+			DB:       0,
+		})
+		if _, err := rdb.Ping(ctx).Result(); err != nil {
+			panic(err)
+		}
+		cache = rdb
+	})
+	return cache
+}
+
+type metricsLogger struct {
+	mu      sync.RWMutex
+	storage *redis.Client
+}
+
+func newMetricsLogger() *metricsLogger {
+	return &metricsLogger{
+		storage: cacheClient(),
+	}
+}
+
+// server_1 INCR
+func (m *metricsLogger) requestHit(name string) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	ctx := context.Background()
+	m.storage.Incr(ctx, fmt.Sprintf("%s:requestCount", name))
+}
+
+// time taken for server_1 to complete the request and send results back
+func (m *metricsLogger) responseTimeLog(name string, time float64) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	ctx := context.Background()
+	m.storage.LPush(ctx, fmt.Sprintf("%s:responseTimes", name), time)
+}
+
+// number of connections that the current server is working with
+func (m *metricsLogger) activeConnections(name string) uint {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	ctx := context.Background()
+	val, _ := m.storage.Get(ctx, fmt.Sprintf("%s:activeConnections", name)).Uint64()
+	return uint(val)
+}
+
+// increment current connection count
+func (m *metricsLogger) addConnection(name string) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	ctx := context.Background()
+	m.storage.Incr(ctx, fmt.Sprintf("%s:activeConnections", name))
+}
+
+// remove connection from current count to keep it updated
+func (m *metricsLogger) removeConnection(name string) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	ctx := context.Background()
+	m.storage.Decr(ctx, fmt.Sprintf("%s:activeConnections", name))
+}
+func (m *metricsLogger) averageResponseTime(name string) float64 {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	ctx := context.Background()
+	val, _ := m.storage.LRange(ctx, fmt.Sprintf("%s:responseTimes", name), 0, -1).Result()
+	var total float64
+	for _, v := range val {
+		if f, err := strconv.ParseFloat(v, 64); err == nil {
+			total += f
+		}
+	}
+	return total / float64(len(val))
+}
+func (m *metricsLogger) stats() interface{} {
+	return 0
+}
+func init() {
+	godotenv.Load()
+}
 func main() {
+	cacheClient()
 	// this is a catch all route
-	workerChannel := make(chan rawRequest, 1024)
+	/*workerChannel := make(chan rawRequest, 1024)
 	onlyHandler := func(w http.ResponseWriter, r *http.Request) {
 		workerChannel <- rawRequest{w: w, r: r}
-	}
+	}*/
+	// (1)
+	// X tbd set up redis instance and manage the server state
+	// Xread from redis instance for server state
+	// Xset up an endpoint to showcase the server stats
+
+	// (2)
+	// X add logging for everything from, who sent what request
+	// X what server is handeling what request
+	// X just basic stats that can be used wih INCR and its vice versa
+	// X we can run aggreagtaions on this stuff later if we want to
+
+	// (3)
+	// make all the IP addresses that this proxy uses the private IP addrerss on the aws VPS
+
+	// (4)
+	// update python test script to use multi theading
+
+	// (5)
+	// write the python server application code that runs in docker, should be simple
+	// and needs to frequenly update redis stats
+
+	// (6)
+	// end to end test,
+	// send request to proxy, proxy -> server , server -> proxy, proxy -> client
+	// latency is track, each servers latency/connections/ect is tracked and updated frequently in redis
+	// < 15 ms of added latency total
+	//
+	/*http.HandleFunc("/stats", stats)
+
 	http.HandleFunc("/", onlyHandler)
+
 	s, algo := initApplicationServers()
 	fmt.Printf("working with the %+v load balancing algorithm\n", algo.name())
 	go handleStream(algo, workerChannel, s)
 
 	fmt.Printf("running on port :79 \n ")
-	http.ListenAndServe(":79", nil)
+	http.ListenAndServe(":79", nil)*/
 
 }
